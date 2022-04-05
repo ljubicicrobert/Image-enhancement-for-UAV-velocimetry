bae86232c3f6fbec248cff375e46af128164e4f5
ljubicicrobert <71554915+ljubicicrobert@users.noreply.github.com>
2022-04-05 20:02:20 +0200

    Update notebooks

--- axes_tiein.py
+++ axes_tiein.py
@@ -1,19 +1,24 @@
+# Used for all single-channel image plots
+# Check out and try different colormaps at:
+# https://matplotlib.org/3.5.1/tutorials/colors/colormaps.html
+cmap = 'viridis'
+
+
 # Callback function to simultaneously zoom all active axes
 def on_lims_change(event_ax):
-	cid_list = list(event_ax.callbacks.callbacks['ylim_changed'].keys())
-	for cid in cid_list:
-		event_ax.callbacks.disconnect(cid)
+    cid_list = list(event_ax.callbacks.callbacks['ylim_changed'].keys())
+    for cid in cid_list:
+        event_ax.callbacks.disconnect(cid)
 
-	fig = event_ax.get_figure()
-	list_ca = fig.axes
+    fig = event_ax.get_figure()
+    list_ca = fig.axes
 
-	xlim = [int(x) for x in event_ax.get_xlim()]
-	ylim = [int(y) for y in event_ax.get_ylim()]
+    xlim = [int(x) for x in event_ax.get_xlim()]
+    ylim = [int(y) for y in event_ax.get_ylim()]
 
-	for a in list_ca:
-		print(a)
-		if a != event_ax:
-			a.set_xlim(xlim)
-			a.set_ylim(ylim)
+    for a in list_ca:
+        if a != event_ax:
+            a.set_xlim(xlim)
+            a.set_ylim(ylim)
 
-	event_ax.callbacks.connect('ylim_changed', on_lims_change)
+    event_ax.callbacks.connect('ylim_changed', on_lims_change)
